5d646d4fdf96a4392c1bc2ea1643e540281398ce
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-03-18 04:36:59 +0700

    fix: code

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ fn main() {
         let stream = stream.unwrap();
         handle_connection(stream);
     }
-
-    fn handle_connection(mut stream: TcpStream) {
-        let buf_reader = BufReader::new(&mut stream);
-        let http_request: Vec<_> = buf_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
-
-        println!("Request: {:#?}", http_request);
-    }
 }
+ 
+fn handle_connection(mut stream: TcpStream) { 
+    let buf_reader = BufReader::new(&mut stream); 
+    let http_request: Vec<_> = buf_reader 
+        .lines() 
+        .map(|result| result.unwrap()) 
+        .take_while(|line| !line.is_empty()) 
+        .collect(); 
+ 
+    println!("Request: {:#?}", http_request); 
+} 
