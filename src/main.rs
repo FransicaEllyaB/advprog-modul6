170fd2ce070148fd6a9da739255c6afa412af3c3
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-03-19 18:03:14 +0700

    (3) Validating request and selectively responding

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,26 @@ fn main() {
  
 fn handle_connection(mut stream: TcpStream) { 
     let buf_reader = BufReader::new(&mut stream); 
-    let http_request: Vec<_> = buf_reader 
-        .lines() 
-        .map(|result| result.unwrap()) 
-        .take_while(|line| !line.is_empty()) 
-        .collect(); 
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
  
-    let status_line = "HTTP/1.1 200 OK"; 
-    let contents = fs::read_to_string("hello.html").unwrap(); 
-    let length = contents.len(); 
- 
-    let response = 
-        format!("{status_line}\r\nContent-Length: 
-{length}\r\n\r\n{contents}"); 
- 
+    let (status_line, filename);
+    if request_line == "GET / HTTP/1.1" {
+        status_line = "HTTP/1.1 200 OK";
+        filename = "hello.html";
+    } else {
+        status_line = "HTTP/1.1 404 NOT FOUND";
+        filename = "404.html";
+    }
+
+    let response = build_response(status_line, filename);
     stream.write_all(response.as_bytes()).unwrap(); 
 } 
+
+fn build_response(status_line: &str, filename: &str) -> String {
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    )
+}
